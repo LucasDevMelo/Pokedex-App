21df5bfe63f402f3647220603717a5f225eae65f
Lucas Lopes <lucaslopesdemelosilva@gmail.com>
2023-11-06 16:21:25 -0300

    Update 06/11/2023

--- app/src/main/java/com/lucasdevmelo/pokedex_app/view/MainActivity.kt
+++ app/src/main/java/com/lucasdevmelo/pokedex_app/view/MainActivity.kt
@@ -2,11 +2,40 @@ package com.lucasdevmelo.pokedex_app.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.lucasdevmelo.pokedex_app.R
+import com.lucasdevmelo.pokedex_app.domain.Pokemon
+import com.lucasdevmelo.pokedex_app.domain.PokemonType
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rvPokemons)
+
+        val charmander = Pokemon(
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+            4,
+            "Charmander",
+            listOf(
+                PokemonType("Fire")
+            )
+        )
+
+        val pokemons = listOf(
+            charmander, charmander, charmander, charmander, charmander
+        )
+
+        val layoutManager = LinearLayoutManager(this)
+
+        recyclerView.layoutManager = layoutManager
+
+        recyclerView.adapter = PokemonAdapter(pokemons)
+
+
+
+
     }
 }
--- app/src/main/java/com/lucasdevmelo/pokedex_app/view/PokemonAdapter.kt
+++ app/src/main/java/com/lucasdevmelo/pokedex_app/view/PokemonAdapter.kt
@@ -35,7 +35,16 @@ class PokemonAdapter(
             val tvType1 = findViewById<TextView>(R.id.tvType1)
             val tvType2 = findViewById<TextView>(R.id.tvType2)
 
+            tvNumber.text = "Número ${item.number}"
+            tvName.text = item.name
+            tvType1.text = item.types[0].name
 
+            if (item.types.size > 1) {
+                tvType2.visibility = View.VISIBLE
+                tvType2.text = item.types[1].name
+            } else {
+                tvType2.visibility = View.GONE
+            }
         }
     }
 
